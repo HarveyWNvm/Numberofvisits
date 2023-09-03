19675b4026ea7255f6f50a72850fc1ff630709b3
HarveyWNvm <60979365+HarveyWNvm@users.noreply.github.com>
2023-09-03 13:24:37 +0300

    numberofvisit.php

--- numberofvisit.php
+++ numberofvisit.php
@@ -1,2 +1,12 @@
-# Numberofvisits
-Basic Number Or Vidit
+<?php
+            session_start(); //start session
+            session_destroy();
+           unset($_SESSION['counter']);
+
+          $_SESSION['visit'] += 1; //set session variable
+
+            //Check if session is set
+           if(isset($_SESSION['visit'])){
+           echo "<b style=\"color:#0d91bd\">You visited this page</b> <span style=\"color:#3dbf15\">".$_SESSION['visit']. "</span> <b>times</b>";
+              }
+            ?>
